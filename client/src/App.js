530066822a19088875cee86584fe45be9f7027e9
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-16 13:39:16 +0100

    Completed Part 2

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import SavedList from './Movies/SavedList';
 import { Route } from 'react-router-dom';
+import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 
 const App = () => {
@@ -14,7 +15,7 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <Route exact path="/" component={} />
+      <Route exact path="/" component={MovieList} />
       <Route path="/movies/:id" component={Movie} />
     </div>
   );
