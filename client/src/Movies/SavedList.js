0fb45419c76c3cc0b0cee2cfb91cd063502cb4e2
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-16 13:56:27 +0100

    Stretch: Used NavLinks instead of spans for the SavedList

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const SavedList = props => (
 	<div className="saved-list">
 		<h3>Saved Movies:</h3>
 		{props.list.map(movie => (
-			<span key={movie.id} className="saved-movie">{movie.title}</span>
+			<NavLink
+				key={movie.id}
+				to={`/movies/${movie.id}`}
+				className="saved-movie">
+				{movie.title}
+			</NavLink>
 		))}
 		<Link to="/" className="home-button">Home</Link>
 	</div>
