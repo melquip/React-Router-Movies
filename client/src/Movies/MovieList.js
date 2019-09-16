103a6e2d1497626b116d5601476f44b1cd8e771b
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-16 13:44:11 +0100

    Stretch: Refactored code be DRY

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 const MovieList = props => {
 	const [movies, setMovies] = useState([])
@@ -23,31 +23,9 @@ const MovieList = props => {
 	return (
 		<div className="movie-list">
 			{movies.map(movie => (
-				<MovieDetails key={movie.id} movie={movie} />
+				<MovieCard key={movie.id} movie={movie} />
 			))}
 		</div>
-	);
-}
-
-function MovieDetails({ movie }) {
-	const { title, director, metascore, stars, id } = movie;
-	return (
-		<Link to={`/movies/${id}`} className="movie-card">
-			<h2>{title}</h2>
-			<div className="movie-director">
-				Director: <em>{director}</em>
-			</div>
-			<div className="movie-metascore">
-				Metascore: <strong>{metascore}</strong>
-			</div>
-			<h3>Actors</h3>
-
-			{stars.map(star => (
-				<div key={star} className="movie-star">
-					{star}
-				</div>
-			))}
-		</Link>
 	);
 }
 
