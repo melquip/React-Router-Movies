5d7cf1ace4dd6f033e1587ad7d88d56139d46887
Melquisedeque Pereira <melqui1@live.com.pt>
2019-09-16 13:53:03 +0100

    Stretch: Added Save Movie functionality

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <Link to="/" className="home-button">Home</Link>
-  </div>
+	<div className="saved-list">
+		<h3>Saved Movies:</h3>
+		{props.list.map(movie => (
+			<span key={movie.id} className="saved-movie">{movie.title}</span>
+		))}
+		<Link to="/" className="home-button">Home</Link>
+	</div>
 );
 
 export default SavedList;
